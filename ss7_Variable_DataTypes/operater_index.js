c275cda240750b76639f7282346c556af544af25
HinNgyn <hiennguyenhuu83@gmail.com>
2023-04-17 20:48:22 +0700

    .

---
+++ ss7_Variable_DataTypes/operater_index.js
@@ -0,0 +1,15 @@
+a = prompt('Enter value of width:')
+b = prompt('Enter value of height:')
+
+let width = parseInt(a)
+let height = parseInt(b)
+
+if(a>0 && b>0){
+    let area = a*b
+
+    document.write('The width is ' + a + '<br/>' + 'The height is ' + b)
+    document.write('<br/>The rectangular area is ' + area)
+    
+}else{
+    alert('Error!')
+}
